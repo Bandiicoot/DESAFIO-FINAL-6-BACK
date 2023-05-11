d1ce359bc8a759d227d75e1bdea16fb9a093e77a
unknown <fa-ndo@hotmail.com>
2023-05-11 16:35:15 -0300

    enviroments

--- server/db.ts
+++ server/db.ts
@@ -1,12 +1,12 @@
 import admin from "firebase-admin";
-import * as serviceAccount from "../key.json"; //Acordate que Axel es Boiviano
+// import * as serviceAccount from "../key.json"; //Acordate que Axel es Boiviano
 
 // console.log(admin);
 // console.log(serviceAccount);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: "https://m6-cap3-default-rtdb.firebaseio.com",
+  credential: admin.credential.cert(process.env.SERVICE_ACCOUNT as any),
+  databaseURL: process.env.DB_URL,
 });
 
 const baseDeDatos = admin.firestore();

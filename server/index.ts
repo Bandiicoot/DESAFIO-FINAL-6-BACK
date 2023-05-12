0cfde4e221d470aa553a537ddd451ac28d057f6a
unknown <fa-ndo@hotmail.com>
2023-05-12 14:29:13 -0300

    un cambio corto

--- server/index.ts
+++ server/index.ts
@@ -72,6 +72,7 @@ app.post("/auth", (req, res) => {
 
 app.post("/createGameRoom", (req, res) => {
   const { userId } = req.body;
+  console.log("llega esto al back:", userId);
   userCollection
     .doc(userId.toString())
     .get()

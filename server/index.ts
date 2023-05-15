bd1c5eedba17ee9903285d91ed475b86edd020b2
unknown <fa-ndo@hotmail.com>
2023-05-15 17:12:45 -0300

    console log

--- server/index.ts
+++ server/index.ts
@@ -72,7 +72,7 @@ app.post("/auth", (req, res) => {
 
 app.post("/createGameRoom", (req, res) => {
   const { userId, userName } = req.body;
-  console.log("llega esto al back:", userId);
+  console.log("llega esto al back:", userId, "Este es el userNme:", userName);
   userCollection
     .doc(userId.toString())
     .get()
